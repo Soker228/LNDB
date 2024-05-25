8f246f2f7249dd27648f7c1d4b7c997496c0cf17
de <lishangde228@163.com>
2024-05-25 16:54:45 +0800

    LNDB 搭建数据库项目

--- src/main/java/top/guoziyang/mydb/backend/dm/logger/Logger.java
+++ src/main/java/top/guoziyang/mydb/backend/dm/logger/Logger.java
@@ -11,12 +11,20 @@ import top.guoziyang.mydb.backend.utils.Panic;
 import top.guoziyang.mydb.backend.utils.Parser;
 import top.guoziyang.mydb.common.Error;
 
+// MYDB提供数据库崩溃后的恢复功能，DM 模块在每次对底层数据操作时，都会记录一条日志到磁盘上。
+// 在数据库崩溃之后（即第一页校验不对时），再次启动时，可以根据日志的内容，恢复数据文件，保证其一致性。
+/**
+ * 日志接口
+ * 默认提供两个静态函数：
+ *      create(String path)：创建日志文件和打开日志功能
+ *      open(String path)：打开日志文件和打开日志功能
+ */
 public interface Logger {
-    void log(byte[] data);
-    void truncate(long x) throws Exception;
-    byte[] next();
-    void rewind();
-    void close();
+    void log(byte[] data);                  // 写入一条日志记录
+    void truncate(long x) throws Exception; // 删除日志文件中x指针后面的文件数据
+    byte[] next();                          // logger设计为一个迭代器，next()获取下一条日志，返回的是日志中的DATA数据
+    void rewind();                          // 将文件指针指向第一条日志
+    void close();                           // 关闭日志
 
     public static Logger create(String path) {
         File f = new File(path+LoggerImpl.LOG_SUFFIX);
@@ -37,7 +45,7 @@ public interface Logger {
             raf = new RandomAccessFile(f, "rw");
             fc = raf.getChannel();
         } catch (FileNotFoundException e) {
-           Panic.panic(e);
+            Panic.panic(e);
         }
 
         ByteBuffer buf = ByteBuffer.wrap(Parser.int2Byte(0));
@@ -48,7 +56,7 @@ public interface Logger {
         } catch (IOException e) {
             Panic.panic(e);
         }
-
+        // 打开日志功能，总校验和初始值为0
         return new LoggerImpl(raf, fc, 0);
     }
 
@@ -67,10 +75,12 @@ public interface Logger {
             raf = new RandomAccessFile(f, "rw");
             fc = raf.getChannel();
         } catch (FileNotFoundException e) {
-           Panic.panic(e);
+            Panic.panic(e);
         }
 
+        // 打开日志功能，不需要设定总校验和的初始值
         LoggerImpl lg = new LoggerImpl(raf, fc);
+        // 日志初始化
         lg.init();
 
         return lg;
--- src/main/java/top/guoziyang/mydb/backend/dm/page/PageX.java
+++ src/main/java/top/guoziyang/mydb/backend/dm/page/PageX.java
@@ -6,41 +6,68 @@ import top.guoziyang.mydb.backend.dm.pageCache.PageCache;
 import top.guoziyang.mydb.backend.utils.Parser;
 
 /**
- * PageX管理普通页
+ * PageX 普通页管理器
+ * 提供了一个由静态方法构成的工具类，用来对普通页管理。
+ * 一个普通页面以一个 2 字节无符号数起始，表示这一页指针的偏移（即这一页写到了哪个位置）。
+ * 剩下的部分都是实际存储的数据。所以对普通页的管理，基本都是围绕着对 FSO（Free Space Offset），也就是接下来往哪里插入数据进行的。
+ *
  * 普通页结构
  * [FreeSpaceOffset] [Data]
  * FreeSpaceOffset: 2字节 空闲位置开始偏移
+ * Data: 由于都是使用DataItem进行数据操作，索引Data的格式就是DataItem的格式：[ValidFlag] [DataSize] [Data]
  */
 public class PageX {
-    
-    private static final short OF_FREE = 0;
-    private static final short OF_DATA = 2;
-    public static final int MAX_FREE_SPACE = PageCache.PAGE_SIZE - OF_DATA;
 
+    private static final short OF_FREE = 0;                                 // 数据页的起始位置
+    private static final short OF_DATA = 2;                                 // 2字节
+    public static final int MAX_FREE_SPACE = PageCache.PAGE_SIZE - OF_DATA; // 一页的最大剩余空间
+
+    /**
+     * 初始化一个空的 8k 页面，设置FSO为2字节
+     * @return 空的数据页
+     */
     public static byte[] initRaw() {
         byte[] raw = new byte[PageCache.PAGE_SIZE];
-        setFSO(raw, OF_DATA);
+        setFSO(raw, OF_DATA); // 初始化的第一个FSO就2字节
         return raw;
     }
 
+    // 在写入之前获取 FSO，来确定写入的位置，并在写入之后更新 FSO
+    /**
+     * 设置数据页前2个字节中存的数据位置偏移量（FSO）
+     * @param raw 数据
+     * @param ofData 偏移量
+     */
     private static void setFSO(byte[] raw, short ofData) {
         System.arraycopy(Parser.short2Byte(ofData), 0, raw, OF_FREE, OF_DATA);
     }
 
-    // 获取pg的FSO
+    /**
+     * 获取数据页前 2 个字节中存的数据位置偏移量（FSO）
+     * @param pg 数据页
+     * @return
+     */
     public static short getFSO(Page pg) {
         return getFSO(pg.getData());
     }
-
     private static short getFSO(byte[] raw) {
         return Parser.parseShort(Arrays.copyOfRange(raw, 0, 2));
     }
 
-    // 将raw插入pg中，返回插入位置
+    /**
+     * 向页面中插入数据
+     * 将raw数据插入pg中，返回插入位置
+     * @param pg 目标数据页
+     * @param raw 插入的数据（DataItem格式的数据包）
+     * @return 插入位置
+     */
     public static short insert(Page pg, byte[] raw) {
         pg.setDirty(true);
+        // 获取该数据页的空闲位置偏移量（FSO）
         short offset = getFSO(pg.getData());
+        // 将raw的内容插入pg数据页的指定位置
         System.arraycopy(raw, 0, pg.getData(), offset, raw.length);
+        // 更新数据页前2个字节的FSO值
         setFSO(pg.getData(), (short)(offset + raw.length));
         return offset;
     }
@@ -50,7 +77,14 @@ public class PageX {
         return PageCache.PAGE_SIZE - (int)getFSO(pg.getData());
     }
 
-    // 将raw插入pg中的offset位置，并将pg的offset设置为较大的offset
+
+    // 以下剩余两个函数 recoverInsert() 和 recoverUpdate() 用于在数据库崩溃后重新打开时（即第一页的校验不对时），恢复例程直接插入数据以及修改数据使用。
+    /**
+     * 直接插入数据页，将raw数据插入pg中的指定的offset位置，更新FSO(将pg的offset设置为较大的offset)
+     * @param pg
+     * @param raw
+     * @param offset
+     */
     public static void recoverInsert(Page pg, byte[] raw, short offset) {
         pg.setDirty(true);
         System.arraycopy(raw, 0, pg.getData(), offset, raw.length);
@@ -61,7 +95,13 @@ public class PageX {
         }
     }
 
-    // 将raw插入pg中的offset位置，不更新update
+    //TODO 为什么offset变了却不更新?
+    /**
+     * 修改数据页，将raw插入pg中的offset位置，不更新FSO
+     * @param pg
+     * @param raw
+     * @param offset
+     */
     public static void recoverUpdate(Page pg, byte[] raw, short offset) {
         pg.setDirty(true);
         System.arraycopy(raw, 0, pg.getData(), offset, raw.length);
