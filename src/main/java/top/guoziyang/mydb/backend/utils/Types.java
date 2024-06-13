a81762c11fa20ae972c7aa67e983556b69f831a5
username <lishangde228@163.com>
2024-06-13 16:21:17 +0800

    更新注释

--- src/main/java/top/guoziyang/mydb/backend/utils/Types.java
+++ src/main/java/top/guoziyang/mydb/backend/utils/Types.java
@@ -7,6 +7,6 @@ public class Types {
     public static long addressToUid(int pgno, short offset) {
         long u0 = (long)pgno;
         long u1 = (long)offset;
-        return u0 << 32 | u1; //或运算全 0 则 0, 见 1 则 1
+        return u0 << 32 | u1;        //或运算全 0 则 0, 见 1 则 1
     }
 }
