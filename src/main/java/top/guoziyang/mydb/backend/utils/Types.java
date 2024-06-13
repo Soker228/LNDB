147e7bc64817063d025956a9b184da266be351b2
username <email>
2024-06-13 16:07:34 +0800

    更新注释

--- src/main/java/top/guoziyang/mydb/backend/utils/Types.java
+++ src/main/java/top/guoziyang/mydb/backend/utils/Types.java
@@ -1,9 +1,12 @@
 package top.guoziyang.mydb.backend.utils;
 
+/**
+ * 页号 + 偏移量
+ */
 public class Types {
     public static long addressToUid(int pgno, short offset) {
         long u0 = (long)pgno;
         long u1 = (long)offset;
-        return u0 << 32 | u1; //或运算是全0则0，见1则1
+        return u0 << 32 | u1; //或运算全 0 则 0, 见 1 则 1
     }
 }
